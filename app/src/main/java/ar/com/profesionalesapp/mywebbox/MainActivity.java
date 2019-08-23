e0dd7336909a6358b644ca6d8c7efb37d8935815
uzeltinger <fabiouz@gmail.com>
2019-08-23 09:49:57 -0300

    inicio

---
+++ app/src/main/java/ar/com/profesionalesapp/mywebbox/MainActivity.java
@@ -0,0 +1,14 @@
+package ar.com.profesionalesapp.mywebbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
